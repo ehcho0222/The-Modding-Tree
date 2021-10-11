a131e74fae37607444d115cb2f5901e41ee2bfc4
ddongddo <choeunho0222@gmail.com>
2021-10-11 19:42:23 +0900

    .

--- js/mod.js
+++ js/mod.js
@@ -1,20 +1,25 @@
 let modInfo = {
-	name: "The GFRIEND Tree",
-	id: "gfriendforever",
+	name: "The Creation Tree",
+	id: "fuckyoucubeentertainment",
 	author: "sleepground123",
-	pointsName: "Popularity",
-	modFiles: ["tree.js", "changelog.js", "layers/ach.js", "layers/story.js", "layers/money.js", "layers/g.js", "layers/f.js"],
+	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (1), // Used for hard resets and new players
-	offlineLimit: 0,  // In hours
+	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	offlineLimit: 1,  // In hours
 }
 
+// Set your version in num and name
 let VERSION = {
-	num: "0.25",
-	name: "Fans Update Part 1",
+	num: "0.9.01",
+	name: "",
 }
+
+let changelog = `<h1>Changelog:</h1><br>
+	<h3>v0.9.01</h3><br>
+		- Added things.<br>
+		- Added stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -28,28 +33,15 @@ function getStartPoints(){
 
 // Determines if it should show points/sec
 function canGenPoints(){
-	return hasUpgrade('money', 12)
+	return true
 }
 
 // Calculate points/sec!
 function getPointGen() {
-	if(!canGenPoints()) return new Decimal(0)
-	let gain = new Decimal(0.05)
-	if (hasUpgrade('g', 25)) gain = gain.times(upgradeEffect('g', 25))
-	if (hasUpgrade('money', 14)) gain = gain.times(upgradeEffect('money', 14))
-	if (hasUpgrade('money', 15)) gain = gain.times(upgradeEffect('money', 15))
-	if (hasUpgrade('money', 21) && gain.gt(1)) gain = gain.pow(upgradeEffect('money', 21))
-	gain = gain.times(buyableEffect('money', 11))
-	gain = gain.times(buyableEffect('money', 12))
-	if (player.g.unlocked) gain = gain.times(tmp.g.effect)
-	if (hasUpgrade('g', 13)) gain = gain.times(upgradeEffect('g', 13))
-	if (hasUpgrade('g', 14)) gain = gain.times(upgradeEffect('g', 14))
-	if (hasUpgrade('g', 31)) gain = gain.times(upgradeEffect('g', 31))
-	if (hasUpgrade('g', 32)) gain = gain.times(upgradeEffect('g', 32))
-	if (hasUpgrade('g', 33)) gain = gain.times(upgradeEffect('g', 33))
-	if (hasUpgrade('g', 34)) gain = gain.times(upgradeEffect('g', 34))
-	if (hasUpgrade('g', 35)) gain = gain.times(upgradeEffect('g', 35))
-	if (hasMilestone('g', 3)) gain = gain.times(player.g.salesEffect)
+	if(!canGenPoints())
+		return new Decimal(0)
+
+	let gain = new Decimal(1)
 	return gain
 }
 
@@ -57,32 +49,13 @@ function getPointGen() {
 function addedPlayerData() { return {
 }}
 
-// GFRIEND Disbandment Clock
-const zeroTime = 1621609200000 // 2021/05/22 00:00:00
-const perDay = 86400000 // milliseconds per day
-
-// Calculate the # of days since disbandment
-function calculateDay() {
-	let time = Date.now()
-	time = time - zeroTime
-	time = Math.floor(time / perDay)
-	return time
-}
-
-function formatDay() {
-	let time = calculateDay()
-	return "GFRIEND Disbandment D+"+time
-}
-
 // Display extra things at the top of the page
 var displayThings = [
-	formatDay(),
-	"Current Endgame: 1e380 Money and 1,000 Fans",
 ]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.money.points.gte(new Decimal("1e380")) && player.f.points.gte(1000)
+	return player.points.gte(new Decimal("e280000000"))
 }
 
 
@@ -96,7 +69,7 @@ var backgroundStyle = {
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(1000)
+	return(3600) // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
